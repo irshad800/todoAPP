010c3358821d534e2c2c7b2d500691ab042fd013
MUHAMMED IRSHAD VP <irshadvp800@gmail.com>
2024-08-01 21:13:32 +0530

    completed

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../bloc/task_bloc.dart';
 import '../model/task.dart';
 import '../screens/edit_task.dart';
+import '../services/notification_service.dart'; // Import NotificationService
 
 class TaskTile extends StatelessWidget {
   final Task task;
@@ -23,28 +24,60 @@ class TaskTile extends StatelessWidget {
         : context.read<TasksBloc>().add(RemoveTask(task: task));
   }
 
+  void _notifyCompletion(BuildContext context) {
+    final notificationService = InheritedNotificationService.of(context);
+    notificationService.showNotification('Task Completed',
+        'The task "${task.title}" has been marked as completed.');
+  }
+
+  Color _getBackgroundColor() {
+    if (task.isPinned) {
+      return Colors.blue.withOpacity(0.1);
+    } else if (task.isDone!) {
+      return Colors.green.withOpacity(0.1);
+    } else {
+      return Colors.grey.withOpacity(0.1);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
-        color: Colors.white.withOpacity(0.1),
+        color: _getBackgroundColor(),
       ),
       child: ListTile(
-        title: Text(
-          task.title,
-          style: TextStyle(
-            decoration: task.isDone! ? TextDecoration.lineThrough : null,
-          ),
+        leading: Checkbox(
+          value: task.isDone,
+          onChanged: (bool? value) {
+            final updatedTask = task.copyWith(isDone: value);
+            onTaskToggle(updatedTask);
+            if (value == true) {
+              _notifyCompletion(context); // Notify when the task is completed
+            }
+          },
         ),
-        leading: Text(
+        title: Column(
+          children: [
+            Text(
+              task.title,
+              style: TextStyle(
+                fontFamily: "Airbnb",
+                color: Colors.black,
+                decoration: task.isDone! ? TextDecoration.lineThrough : null,
+              ),
+            ),
+            Text(task.details)
+          ],
+        ),
+        subtitle: Text(
           task.dueDate != null
               ? '${task.dueDate!.toLocal()}'.split(' ')[0]
               : 'No Date',
           style: TextStyle(color: Colors.grey),
         ),
-        subtitle: Text(task.details),
         trailing: PopupMenuButton<String>(
           onSelected: (value) {
             if (value == 'edit') {
